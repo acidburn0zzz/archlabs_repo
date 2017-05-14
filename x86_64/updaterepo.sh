2394c74ecc6a22936bdf3089971a9375e14e4463
Erik Dubois <erik.dubois@gmail.com>
2017-05-14 09:22:03 +0200

    update

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 repo-add archlabs_repo.db.tar.gz *.pkg.tar.xz
-sleep 2
+sleep 4
 rm archlabs_repo.db
-sleep 2
+sleep 4
 cp archlabs_repo.db.tar.gz archlabs_repo.db
+
+echo "####################################"
 echo "Repo Updated!!"
+echo "####################################"
